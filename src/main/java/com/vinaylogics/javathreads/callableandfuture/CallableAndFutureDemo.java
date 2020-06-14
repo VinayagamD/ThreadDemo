afa852b475f9d8116e1bf2634d4adf3d7900699f
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-06-14 10:28:06 +0530

    Callable And Future Demo

---
+++ src/main/java/com/vinaylogics/javathreads/callableandfuture/CallableAndFutureDemo.java
@@ -0,0 +1,52 @@
+package com.vinaylogics.javathreads.callableandfuture;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class CallableAndFutureDemo {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // FutureTask is a concrete class that
+        // implements both Runnable and Future
+        FutureTask[] randomNumberTasks = new FutureTask[5];
+        for (int i = 0; i < 5; i++)
+        {
+            Callable callable = new MyCallable();
+
+            // Create the FutureTask with Callable
+            randomNumberTasks[i] = new FutureTask(callable);
+
+            // As it implements Runnable, create Thread
+            // with FutureTask
+            Thread t = new Thread(randomNumberTasks[i]);
+            t.start();
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            // As it implements Future, we can call get()
+            System.out.println(randomNumberTasks[i].get());
+
+            // This method blocks till the result is obtained
+            // The get method can throw checked exceptions
+            // like when it is interrupted. This is the reason
+            // for adding the throws clause to main
+        }
+
+    }
+
+    public static class MyCallable implements Callable{
+
+        @Override
+        public Object call() throws Exception {
+            Random generator = new Random();
+            Integer randomNumber = generator.nextInt(5);
+
+            Thread.sleep(randomNumber * 1000);
+
+            return randomNumber;
+        }
+    }
+}
